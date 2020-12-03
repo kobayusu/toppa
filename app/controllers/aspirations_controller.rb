bac4f33579fef362a5c01b5888799ca611214478
kobayusu <cb3ysk@gmail.com>
2020-12-03 19:14:42 +0900

    目標設定画面

--- app/controllers/aspirations_controller.rb
+++ app/controllers/aspirations_controller.rb
@@ -7,4 +7,12 @@ class AspirationsController < ApplicationController
     @aspiration = Aspiration.new
   end
   
+  def create 
+    Aspiration.create(aspiration_params)
+  end
+
+  private
+  def aspiration_params
+    params.require(:tweet).permit(:title, :detail, :deadline, :category,)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'aspirations#index'
-  resources :aspirations, only: [:index, :new]
+  resources :aspirations, only: [:index, :new, :create]
 end
