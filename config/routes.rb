5162ad7a5562bda01c7244c99659071ccb00844d
kobayusu <cb3ysk@gmail.com>
2021-02-18 23:36:46 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'aspirations#index'
   resources :aspirations
-  resources :records #, only: [:create ,:new, :edit, :show, :update, :destroy]
+  resources :records 
 end
